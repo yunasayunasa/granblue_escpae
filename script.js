2d459799f55d98ee1abbd619987a086baa0b8d32
yunasayunasa <198059552+yunasayunasa@users.noreply.github.com>
2025-02-20 21:57:47 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -497,7 +497,7 @@ if (driveArea2) {
         const c1 = createCharacterElement(charDefs[0]);
         const c2 = createCharacterElement(charDefs[1]);
         c1.style.position = "absolute";
-        c1.style.left = "50%";
+        c1.style.left = "80%";
         c2.style.position = "absolute";
         c2.style.right = "80%";
         characterContainer.appendChild(c1);
@@ -524,12 +524,12 @@ if (charDef.name === "グラン") {
 } else if (charDef.name === "ロベリア") {
   img.style.maxWidth = "80%";
 } else if (charDef.name === "ノア") {
-  img.style.maxWidth ="40%";
+  img.style.maxWidth ="80%";
 } else if (charDef.name === "ロベリアネガ" || charDef.name === "ノアネガ") {
   // 敵キャラの場合、ここで設定
   img.style.maxWidth = "80%";
 } else {
-  img.style.maxWidth = "40%";
+  img.style.maxWidth = "20%";
 }
 
  // グランだけ位置が高すぎる場合、追加で下にずらす
